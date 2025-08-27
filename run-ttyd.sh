166c2f110c8368b2f783d20317e276d259dfd1ee
leenchan <leenchan@gmail.com>
2025-08-28 00:50:18 +0800

    debug functions

--- run-ttyd.sh
+++ run-ttyd.sh
@@ -1,30 +1,31 @@
 #!/bin/sh
 CUR_DIR=$(cd $(dirname $0); pwd)
 
+echo "$CUR_DIR/functions.sh"
 source $CUR_DIR/functions.sh
 
 [ -x "$(which ttyd)" ] || {
-  echo "Installing ttyd..."
-  TTYD_DL_URL=$(get_github_release "tsl0922/ttyd" "1.7.4" "x86_64")
-  install_bin "${TTYD_DL_URL}" "$(basename "$TTYD_DL_URL"):ttyd"
+	echo "Installing ttyd..."
+	TTYD_DL_URL=$(get_github_release "tsl0922/ttyd" "1.7.4" "x86_64")
+	install_bin "${TTYD_DL_URL}" "$(basename "$TTYD_DL_URL"):ttyd"
 }
 
 [ -x "$(which ngrok)" ] || {
-  echo "Installing ngrok..."
-  NGROK_DL_URL=$(get_urls_from_html "https://ngrok.com/downloads/linux?tab=download" | grep 'amd64' | head -n1)
-  install_bin "$NGROK_DL_URL" "ngrok"
+	echo "Installing ngrok..."
+	NGROK_DL_URL=$(get_urls_from_html "https://ngrok.com/downloads/linux?tab=download" | grep 'amd64' | head -n1)
+	install_bin "$NGROK_DL_URL" "ngrok"
 }
 
 [ -x "$(which ttyd)" -a -x "$(which ngrok)" ] || exit 1
 
 [ -z "${NGROK_AUTHTOKEN}" ] && {
-  echo "Please set secrets.NGROK_AUTHTOKEN in repo settings"
-  exit 1
+	echo "Please set secrets.NGROK_AUTHTOKEN in repo settings"
+	exit 1
 }
 
 [ -z "${1}" ] && {
-  echo "Please provide a command to run"
-  exit 1
+	echo "Please provide a command to run"
+	exit 1
 }
 
 ttyd -W -p 7681 sh -c "${1}; killall ngrok" &
