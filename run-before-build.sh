283dd972b8134478b6e0066a9d04dfd264ca55ee
leenchan <leenchan@gmail.com>
2025-09-08 18:33:20 +0800

    fix nginx njs module

--- run-before-build.sh
+++ run-before-build.sh
@@ -3,3 +3,8 @@
 # sudo apt install libfuse-dev
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+# Fix compile nginx-mod-njs
+grep -Eq 'NGX_LD_OPT' feeds/packages/net/nginx/patches/nginx-mod-njs/104-endianness_fix.patch || {
+  cp -f $GITHUB_WORKSPACE/nginx/patches/nginx-mod-njs/104-endianness_fix.patch feeds/packages/net/nginx/patches/nginx-mod-njs/104-endianness_fix.patch
+}
