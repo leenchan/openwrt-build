142f5dcd280deb40e8874ffe6a649ed09e3b3d36
leenchan <leenchan@gmail.com>
2025-10-01 15:28:06 +0800

    Update run-before-build.sh

--- run-before-build.sh
+++ run-before-build.sh
@@ -5,6 +5,6 @@
 # git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 # Fix compile nginx-mod-njs
-grep -Eq 'NGX_LD_OPT' feeds/packages/net/nginx/patches/nginx-mod-njs/104-endianness_fix.patch || {
-  cp -f $GITHUB_WORKSPACE/feeds/nginx/patches/nginx-mod-njs/104-endianness_fix.patch feeds/packages/net/nginx/patches/nginx-mod-njs/104-endianness_fix.patch
-}
+# grep -Eq 'NGX_LD_OPT' feeds/packages/net/nginx/patches/nginx-mod-njs/104-endianness_fix.patch || {
+#   cp -f $GITHUB_WORKSPACE/feeds/nginx/patches/nginx-mod-njs/104-endianness_fix.patch feeds/packages/net/nginx/patches/nginx-mod-njs/104-endianness_fix.patch
+# }
