817194c332163cc4d5f1f26d344b1841551ec8b4
leenchen <leenchen@nmg.com.hk>
2026-01-06 10:27:45 +0800

    set github token for cloning repo

--- run-before-update-feeds.sh
+++ run-before-update-feeds.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 git clone https://github.com/sbwml/luci-app-openlist2 package/openlist2
-git clone --recurse-submodules https://github.com/leenchan/openwrt-package-custom.git package/custom && \
+git clone --recurse-submodules https://${GITHUB_PAT_TOKEN}@github.com/leenchan/openwrt-package-custom.git package/custom && \
 	git -C package/custom submodule update --remote
 # Fix compile quickfile for aarch64
 [ -f ./package/custom/quickfile/quickfile/Makefile ] && sed -Ei "s|.*ARCH_PACKAGES.*|ifeq (\$(ARCH),aarch64)\n	\$(INSTALL_BIN) \$(PKG_BUILD_DIR)/quickfile-aarch64_generic \$(1)/usr/bin/quickfile\nelse\n	\$(INSTALL_BIN) \$(PKG_BUILD_DIR)/quickfile-\$(ARCH) \$(1)/usr/bin/quickfile\nendif|g" ./package/custom/quickfile/quickfile/Makefile
